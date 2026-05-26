04b586b4947124fe9d81bbbd9e10e69ea0aa6801
Andres <andramirez23@gmail.com>
2026-05-26 08:13:34 -0500

    mi primer commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+const noResults = document.getElementById("noResults");
+const themeToggle = document.getElementById("themeToggle");
+
+
+// FILTRO DINÁMICO
+
+searchInput.addEventListener("keyup", () => {
+
+    const searchText = searchInput.value.toLowerCase();
+
+    let visibleCards = 0;
+
+    cards.forEach(card => {
+
+        const title = card.querySelector("h2").textContent.toLowerCase();
+
+        const description = card.querySelector("p").textContent.toLowerCase();
+
+        if (
+            title.includes(searchText) ||
+            description.includes(searchText)
+        ) {
+            card.style.display = "block";
+            visibleCards++;
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    if (visibleCards === 0) {
+        noResults.classList.remove("hidden");
+    } else {
+        noResults.classList.add("hidden");
+    }
+});
+
+
+// MODO OSCURO
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️ Modo Claro";
+    } else {
+        themeToggle.textContent = "🌙 Modo Oscuro";
+    }
+});
