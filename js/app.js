1be1b5ad566a0ac3d9e031ece50d3e141dac8eb8
Andres <andramirez23@gmail.com>
2026-05-26 08:36:55 -0500

    hice modificaciones

--- js/app.js
+++ js/app.js
@@ -1,51 +1,85 @@
 const searchInput = document.getElementById("searchInput");
+
 const cards = document.querySelectorAll(".card");
+
 const noResults = document.getElementById("noResults");
+
 const themeToggle = document.getElementById("themeToggle");
 
 
-// FILTRO DINÁMICO
+// =========================
+// FILTRO
+// =========================
 
 searchInput.addEventListener("keyup", () => {
 
-    const searchText = searchInput.value.toLowerCase();
+    const searchText =
+        searchInput.value.toLowerCase();
 
     let visibleCards = 0;
 
     cards.forEach(card => {
 
-        const title = card.querySelector("h2").textContent.toLowerCase();
+        const title =
+            card.querySelector("h2")
+            .textContent
+            .toLowerCase();
 
-        const description = card.querySelector("p").textContent.toLowerCase();
+        const description =
+            card.querySelector("p")
+            .textContent
+            .toLowerCase();
 
-        if (
+        if(
             title.includes(searchText) ||
             description.includes(searchText)
-        ) {
+        ){
+
             card.style.display = "block";
+
             visibleCards++;
-        } else {
+
+        }else{
+
             card.style.display = "none";
+
         }
+
     });
 
-    if (visibleCards === 0) {
+    if(visibleCards === 0){
+
         noResults.classList.remove("hidden");
-    } else {
+
+    }else{
+
         noResults.classList.add("hidden");
+
     }
+
 });
 
 
-// MODO OSCURO
+// =========================
+// DARK MODE
+// =========================
 
 themeToggle.addEventListener("click", () => {
 
     document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        themeToggle.textContent = "☀️ Modo Claro";
-    } else {
-        themeToggle.textContent = "🌙 Modo Oscuro";
+    if(
+        document.body.classList.contains("dark-mode")
+    ){
+
+        themeToggle.textContent =
+            "☀️ Modo Claro";
+
+    }else{
+
+        themeToggle.textContent =
+            "🌙 Modo Oscuro";
+
     }
+
 });
